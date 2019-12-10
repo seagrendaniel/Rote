0f7973c8fc5cd5f4c8dffb3f663e0015bed20c1b
Branden <b.lowe333@gmail.com>
2019-12-09 16:35:18 -0800

    Added more rules to linter, fixed linting errors.

--- src/components/GameWindow/GameWindow.jsx
+++ src/components/GameWindow/GameWindow.jsx
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 import './GameWindow.css';
 
 class GameWindow extends Component {
+  state = {
+    ref: 'canvas'
+  };
+
   render() {
+    const { ref } = this.state;
     return (
       <div className="game-window">
-        <canvas ref="canvas" />
+        <canvas ref={ref} />
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './pages/App/App';
+
 import './index.css';
-import App from './pages/App/App.jsx';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
@@ -14,5 +14,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-
-serviceWorker.unregister();
