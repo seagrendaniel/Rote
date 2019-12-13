512a57bcd294642deb18f38d73711be618b96485
blackdanamite <seagrendaniel562@gmail.com>
2019-12-13 11:29:19 -0800

    code org

--- src/components/GameWindow/GameWindow.jsx
+++ src/components/GameWindow/GameWindow.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import {
+  Layer, Rect, Stage, Group
+} from 'react-konva';
 
 import './GameWindow.css';
 
 class GameWindow extends Component {
   constructor(props) {
     super(props);
-    this.canvasRef = React.createRef();
+    this.canvasRef = React.createRef(); // create ref to be manipulated
   }
 
   componentDidMount() {
     const { canvas } = this.canvasRef;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d'); // ctx is variable to be used in drawing
   }
 
+  /* --- Drawing commands --- */
+
+  /* --- Render function --- */
   render() {
     return (
       <div className="game-window">
